833aa5562860c58c71b9e7658f7fb84064f7769f
Gabriela R <ladyg432@gmail.com>
2023-02-06 20:59:32 -0300

    th text

--- src/main/java/br/com/treinaweb/twclientes/controller/HomeController.java
+++ src/main/java/br/com/treinaweb/twclientes/controller/HomeController.java
@@ -1,7 +1,12 @@
 package br.com.treinaweb.twclientes.controller;
 
+
+import java.util.ArrayList;
+import java.util.List;
+
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.ui.ModelMap;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -9,13 +14,23 @@ import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.servlet.ModelAndView;
 
+
 @Controller
 @RequestMapping("/")
 public class HomeController {
 
    // @RequestMapping(path = "/", method   = RequestMethod.GET) pode ser utlizado ou o GetMapping
    @GetMapping("/")
-    public String home() {
+    public String home(ModelMap model) {
+        model.addAttribute("boasvindas", "Bem vindo ao curso de Spring com Thymeleaf!");
+
+        List<String> aulas = new ArrayList<>();
+        aulas.add("Aula 01 - Introdução");
+        aulas.add("Aula 02 - Templante engines");
+        aulas.add("Aula 03 - Arquivos estáticos");
+
+        model.addAttribute("aulas", aulas);
+        
         return "home";
     }
     

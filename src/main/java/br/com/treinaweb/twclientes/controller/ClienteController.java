932ae37694e065edf95274d47ef2f1c7806f2d1c
Gabriela R <ladyg432@gmail.com>
2023-02-12 16:31:12 -0300

    Adicionando condicional para apresentar formulário

--- src/main/java/br/com/treinaweb/twclientes/controller/ClienteController.java
+++ src/main/java/br/com/treinaweb/twclientes/controller/ClienteController.java
@@ -55,7 +55,7 @@ public class ClienteController {
 
     @GetMapping("/cadastrar")
     public ModelAndView cadastrar(){
-        ModelAndView modelAndView = new ModelAndView("/cliente/cadastro");
+        ModelAndView modelAndView = new ModelAndView("/cliente/formulario");
 
         modelAndView.addObject("cliente", new Cliente());
 
@@ -73,7 +73,7 @@ public class ClienteController {
 
     @GetMapping("/{id}/editar")
     public ModelAndView editar(@PathVariable Long id){
-        ModelAndView modelAndView = new ModelAndView("cliente/edicao");
+        ModelAndView modelAndView = new ModelAndView("cliente/formulario");
 
         Cliente cliente = clienteRepository.getOne(id);
         modelAndView.addObject("cliente", cliente);
